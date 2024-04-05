ee2894e2d62c82c48d8a6ee505139608e2275f5d
Muraliram <115497046+muraliram516@users.noreply.github.com>
2024-04-05 23:14:14 +0530

    projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -7,19 +7,19 @@ export const projectsData = [
     {
         id: 1,
         name: 'Ganga Systems',
-        description: "I Designed and developed a dynamic computer service Shop website. platform utilizing Reactjs,Implemented user-friendly interfaces, streamlined navigation, and efficient systems to enhance user experience and facilitate seamless interactions for clients seeking computer repair, maintenance, and CCTV support services. https://gangasystems.netlify.app/",
+        description: 'I Designed and developed a dynamic computer service Shop website. platform utilizing Reactjs,Implemented user-friendly interfaces, streamlined navigation, and efficient systems to enhance user experience and facilitate seamless interactions for clients seeking computer repair, maintenance, and CCTV support services.',
         tools: ['Reactjs','Html5','Css3','Javascipt'],
-        role: 'Fronend Developer',
-        demo: 'https://gangasystems.netlify.app/',
+        role: 'ReactJS Developer',
+        demo: "https://gangasystems.netlify.app/",
         code: '',
         image: crefin,
     },
     {
         id: 2,
         name: 'Flipkart Clone',
-        description: 'User Authentication System: Secure login/logout functionality with user registration and when new user signup after signup user should verify their email address.Here i am using supabase for email confirmationShopping Cart: An interactive shopping cart that allows users to add or remove products or buy products. https://kartclone.netlify.app/',
+        description: 'User Authentication System: Secure login/logout functionality with user registration and when new user signup after signup user should verify their email address.Here i am using supabase for email confirmationShopping Cart: An interactive shopping cart that allows users to add or remove products or buy products.',
         tools: ['Reactjs','Redux','Html5','Css3','Javascipt','Supabase'],
-        role: 'Froned Developer',
+        role: 'ReactJS Developer',
         demo: 'https://kartclone.netlify.app/',
         code: '',
         image: travel,
@@ -27,27 +27,12 @@ export const projectsData = [
     {
         id: 3,
         name: 'Trello Task Management',
-        description: 'Dynamic Card Generation: Utilized Reactjs state management to efficiently handle dynamic content creation and updates, including the ability to add, edit, and delete tasks within cards.Drag-and-Drop Interface: This feature significantly improved the user experience by facilitating easy task prioritization and management. https://trelloboards.netlify.app/ ',
+        description: 'Dynamic Card Generation: Utilized Reactjs state management to efficiently handle dynamic content creation and updates, including the ability to add, edit, and delete tasks within cards.Drag-and-Drop Interface: This feature significantly improved the user experience by facilitating easy task prioritization and management.',
         tools: ['Reactjs','Html5','Css3','Javascipt','Firebase'],
-        role: 'Froned Developer',
+        role: 'ReactJS Developer',
         demo: 'https://trelloboards.netlify.app/',
         code: '',
         image: realEstate,
     },
 
 ];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
